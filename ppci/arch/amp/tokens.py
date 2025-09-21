04e67de953c7de9b32324dda520e620a79402951
Sahil Patel <patelsah123456@gmail.com>
2025-09-21 15:03:38 -0400

    Update tokens.py

--- ppci/arch/amp/tokens.py
+++ ppci/arch/amp/tokens.py
@@ -32,19 +32,6 @@ class AmpJToken(Token):
     rs1 = bit_range(25, 32)
     imm20 = bit_range(5, 24)
     schdImm = bit_range(0, 4)
-<<<<<<< HEAD
-    #WIP
-
-class AmpFenceToken(Token):
-    class Info:
-        size = 64
-
-    reserved = bit_range(48, 63)
-    opcode = bit_range(41, 47)
-    #WIP
-
-=======
->>>>>>> 78f4afe0021ccdccd5fd4f632cd9ba68a5970f11
 
 class AmpHaltToken(Token):
     class Info:
@@ -52,7 +39,6 @@ class AmpHaltToken(Token):
 
     reserved = bit_range(48, 63)
     opcode = bit_range(41, 47)
-<<<<<<< HEAD
     #WIP
 
 class AmpNOPToken(Token):
@@ -62,7 +48,5 @@ class AmpNOPToken(Token):
     reserved = bit_range(48, 63)
     opcode = bit_range(41, 47)
     #WIP
-=======
     imm = bit_range(5, 40)
     schdImm = bit_range(0, 4)
->>>>>>> 78f4afe0021ccdccd5fd4f632cd9ba68a5970f11
