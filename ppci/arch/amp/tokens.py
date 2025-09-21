3b23b99dc417548c12b32e51751bc9c94be8ccec
Ivor Chu <41135347+Ivorchu@users.noreply.github.com>
2025-09-21 15:23:44 -0400

    tokens fix

--- ppci/arch/amp/tokens.py
+++ ppci/arch/amp/tokens.py
@@ -1,6 +1,92 @@
-from ..token import Token, bit, bit_concat, bit_range
+from ..token import Token, bit_range
 
-class AmpToken(Token):
+class AmpSDMAToken(Token):
+    class Info:
+        size = 64
+
+    reserved = bit_range(48, 63)
+    opcode = bit_range(41, 47)
+    rd1 = bit_range(33, 40)
+    rs1 = bit_range(25, 32)
+    rs2 = bit_range(17, 24)
+    imm12 = bit_range(5, 16)
+    schdImm = bit_range(0, 4)
+
+class AmpVMToken(Token):
+    class Info:
+        size = 64
+
+    reserved = bit_range(48, 63)
+    opcode = bit_range(41, 47)
+    rd1 = bit_range(33, 40)
+    rs1 = bit_range(25, 32)
+    rs2 = bit_range(17, 24)
+    imm12 = bit_range(5, 16)
+    schdImm = bit_range(0, 4)
+
+class AmpSAMToken(Token):
+    class Info:
+        size = 64
+
+    reserved = bit_range(48, 63)
+    opcode = bit_range(41, 47)
+    rd1 = bit_range(33, 40)
+    rs1 = bit_range(25, 32)
+    rs2 = bit_range(17, 24)
+    imm12 = bit_range(5, 16)
+    schdImm = bit_range(0, 4)
+
+class AmpTCAToken(Token):
+    class Info:
+        size = 64
+
+    reserved = bit_range(48, 63)
+    opcode = bit_range(41, 47)
+    rd1 = bit_range(33, 40)
+    rs1 = bit_range(25, 32)
+    rs2 = bit_range(17, 24)
+    imm12 = bit_range(5, 16)
+    schdImm = bit_range(0, 4)
+
+
+# Scalar
+class AmpRToken(Token):
+    class Info:
+        size = 64
+
+    reserved = bit_range(48, 63)
+    opcode = bit_range(41, 47)
+    rd1 = bit_range(33, 40)
+    rs1 = bit_range(25, 32)
+    rs2 = bit_range(17, 24)
+    imm12 = bit_range(5, 16)
+    schdImm = bit_range(0, 4)
+
+class AmpBRToken(Token):
+    class Info:
+        size = 64
+
+    reserved = bit_range(48, 63)
+    opcode = bit_range(41, 47)
+    rd1 = bit_range(33, 40)
+    rs1 = bit_range(25, 32)
+    rs2 = bit_range(17, 24)
+    imm12 = bit_range(5, 16)
+    schdImm = bit_range(0, 4)
+
+class AmpIToken(Token):
+    class Info:
+        size = 64
+
+    reserved = bit_range(48, 63)
+    opcode = bit_range(41, 47)
+    rd1 = bit_range(33, 40)
+    rs1 = bit_range(25, 32)
+    rs2 = bit_range(17, 24)
+    imm12 = bit_range(5, 16)
+    schdImm = bit_range(0, 4)
+
+class AmpMemToken(Token):
     class Info:
         size = 64
 
@@ -33,6 +119,16 @@ class AmpJToken(Token):
     imm20 = bit_range(5, 24)
     schdImm = bit_range(0, 4)
 
+class AmpFenceToken(Token):
+    class Info:
+        size = 64
+
+    reserved = bit_range(48, 63)
+    opcode = bit_range(41, 47)
+    #WIP
+    imm = bit_range(5, 40)
+    schdImm = bit_range(0, 4)
+
 class AmpHaltToken(Token):
     class Info:
         size = 64
@@ -40,6 +136,8 @@ class AmpHaltToken(Token):
     reserved = bit_range(48, 63)
     opcode = bit_range(41, 47)
     #WIP
+    imm = bit_range(5, 40)
+    schdImm = bit_range(0, 4)
 
 class AmpNOPToken(Token):
     class Info:
