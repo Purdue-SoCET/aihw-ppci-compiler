5708219248a54b777e853f8b060a78847220ccac
Ivor Chu <41135347+Ivorchu@users.noreply.github.com>
2025-09-21 14:22:59 -0400

    tokens

--- ppci/arch/amp/tokens.py
+++ ppci/arch/amp/tokens.py
@@ -6,8 +6,29 @@ class AmpToken(Token):
 
     reserved = bit_range(48, 63)
     opcode = bit_range(41, 47)
-    rd = bit_range(33, 40)
+    rd1 = bit_range(33, 40)
     rs1 = bit_range(25, 32)
     rs2 = bit_range(17, 24)
     imm12 = bit_range(5, 16)
+    schdImm = bit_range(0, 4)
+
+class AmpMIToken(Token):
+    class Info:
+        size = 64
+
+    reserved = bit_range(48, 63)
+    opcode = bit_range(41, 47)
+    rd1 = bit_range(33, 40)
+    imm28 = bit_range(5, 32)
+    schdImm = bit_range(0, 4)
+
+class AmpMIToken(Token):
+    class Info:
+        size = 64
+
+    reserved = bit_range(48, 63)
+    opcode = bit_range(41, 47)
+    rd1 = bit_range(33, 40)
+    rs1 = bit_range(25, 32)
+    imm20 = bit_range(5, 24)
     schdImm = bit_range(0, 4)
