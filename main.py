ce1b3a3154ee884864ccd08c006efac799e4913e
Mikhail Golovenchits <golovenchitc@gmail.com>
2025-10-05 16:34:41 -0400

    branch fix

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from ppci.api import cc, link, ir_to_assembly
 from ppci.lang.c import c_to_ir
 from ppci.ir import Module
 from ppci.binutils.objectfile import print_object
+from ppci.arch.amp.asm_printer import AtallaAsmPrinter
 
 def main():
     with open("sample.c", "r") as f:
-        cc(f, "atalla")
+        reporter = AtallaAsmPrinter()
+        reporter.print_instruction(cc(f, "atalla"))
 
 
 if __name__ == "__main__":
