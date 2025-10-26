885a3579a8f5f007211fdc8bcc959b0f98d6e617
Mikhail Golovenchits <golovenchitc@gmail.com>
2025-10-26 15:29:39 -0400

    fixed conflicts, added amps to gitignore

--- sample.c
+++ sample.c
@@ -2,15 +2,7 @@
 
 
 int main() {
-<<<<<<< HEAD
-   int input1 = 10;
-   int input2 = 20;
-   int output = 0;
-   output = (input1 + input2) > 30 ? 1 : 2;
-   // theta(output, input1, input2);
-=======
     int x = 1;
     int y = 2;
     return x + y;
->>>>>>> 722e94b5 (printing ir dag)
 }
