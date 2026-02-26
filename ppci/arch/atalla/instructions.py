4838b7f85de2cc7472cf5e6ce845f3218b7bfcea
prowl-113 <j.erwin424221@gmail.com>
2026-02-26 11:15:41 -0500

    Trying to test branching relocations, got branches to emit

--- ppci/arch/atalla/instructions.py
+++ ppci/arch/atalla/instructions.py
@@ -139,7 +139,6 @@ class AtallaBRInstruction(Instruction):
     tokens = [AtallaBRToken]
     isa = isa
 
-
 class BranchBase(AtallaBRInstruction):
     def relocations(self):
         return [AtallaBR_Imm10_Relocation(self.imm10)]
@@ -165,8 +164,10 @@ def make_br(mnemonic, opcode):
         "imm10": imm10,
         # "incr_imm7": incr_imm7, #TODO: what is this and how to use
         "opcode": opcode,
+        "patterns": patterns,
+        "tokens" : tokens
     }
-    return type(mnemonic + "_ins", (BranchBase,), members)
+    return type(mnemonic + "_ins", (AtallaBRInstruction,), members)
 
 # These are the opcodes that are in the Atalla ISA sheet - James
 Beqs = make_br("beq_s", 0b0100011)  # WAS: 0b0001110
